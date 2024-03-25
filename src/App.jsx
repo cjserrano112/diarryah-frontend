d8accfbf19eeb1d9d59da8c29091cff93d8246a0
cjserrano112 <christopher.serrano@stud.fh-campuswien.ac.at>
2024-03-25 21:25:38 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './css/App.css'
+
+function App() {
+  const projectName = "Dear Diarryah"
+
+  return (
+    <>
+      <div>
+        <h1>{projectName}</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
